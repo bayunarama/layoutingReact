150f2e2c2dcbfd0a9dfa2e9a0f4229cc4ee96ca3
bayunarama <bayunarama26@gmail.com>
2021-10-02 23:03:02 +0700

    second commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./assets/Style.css";
-
+import { FiMenu } from "react-icons/fi";
 const Header = () => {
   return (
     <div className="Header">
@@ -18,15 +18,29 @@ const Header = () => {
 
       <header>
         <div className="container-header">
-          <div className="brand"> 
+          <div className="brand">
             <img src="git.png " alt="" width="40px" />
-            </div>
-          <div className="links">
-            <a href="/home">My Work</a>
-            <a href="/home">About</a>
-            <a href="/home">Design</a>
           </div>
-          <div className="button">Contact Me</div>
+          <div className="links">
+            <ul className="nav-list">
+              <li className="list-item">
+                <a href="/home">My Work</a>{" "}
+              </li>
+              <li>
+                <a href="/home">About</a>
+              </li>
+              <li>
+                {" "}
+                <a href="/home">Design</a>
+              </li>
+              <li>
+                <div className="button">Contact Me</div>
+              </li>
+            </ul>
+          </div>
+          <div className="hamburger-menu">
+            <FiMenu size="24px" />
+          </div>
         </div>
       </header>
     </div>
